7e5f9ff22d61bb88893ee6f6d8844ec51f16d62e
Dongwon <kdw686968@gmail.com>
2024-05-09 11:38:48 +0900

    필요없는 코드 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,5 @@
-function MyComponent() {
-  return (
-    // 최상위 컴포넌트 사용하지 않을 때 -> fragent -> <></>
-    <>
-      <p>Lorem ipsum dolor.</p>
-      <p>Earum, nulla, repudiandae?</p>
-    </>
-  );
-}
-
-function MyComp() {
-  //최상위 컴포넌트는 1개임 div로 감싸주면 해결
-  return (
-    <div>
-      <p>뭐</p>
-      <p>뭐뭣</p>
-    </div>
-  );
-}
-function MyTag() {
-  // jsx 코드가 여러 줄일때 () 사용
-  return (
-    <div>
-      {" "}
-      hello react
-      <p>Lorem ipsum .</p>
-      <p>Aliquid asp</p>
-    </div>
-  );
-}
-
 function App() {
-  return (
-    <div>
-      hello react
-      <MyTag />
-      <MyComp />
-      <MyComponent />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
