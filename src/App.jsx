5e42993973b76d442f9b1e2c50577f7530dee7a4
Dongwon <kdw686968@gmail.com>
2024-05-13 10:19:43 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
 import React from "react";
-import { value1, value2, value3 as v3, value4 } from "./MyValues.jsx";
-import { myAddress, myStr, value3 as vv3 } from "./MyComps.jsx";
 
 function App(props) {
-  return (
-    <div>
-      <div>value1 : {value1}</div>
-      <div>value2 : {value2}</div>
-      <div>value3 : {vv3}</div>
-      <div>v3 : {v3}</div>
-      <div>value4 : {value4.team}</div>
-      <div>myStr : {myStr}</div>
-      <div>myAddress : {myAddress}</div>
-    </div>
-</div>
-)
-  ;
+  return <div></div>;
 }
 
 export default App;
