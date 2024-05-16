2e3b5eb2b07f0f4dda10e81b52c2f24bf80e0f65
Dongwon <kdw686968@gmail.com>
2024-05-16 16:35:07 +0900

    finally 응답코드에 관계없이 항상실행되는 메소드

--- src/App.jsx
+++ src/App.jsx
@@ -37,11 +37,47 @@ function App(props) {
       });
   }
 
+  function handleSome() {
+    const r = Math.random();
+    let path = "";
+    if (r < 1 / 3) {
+      path = "/api/main43/sub1";
+    } else if (r < 2 / 3) {
+      path = "/api/main43/sub3";
+    } else {
+      path = "/api/main43/sub4";
+    }
+    axios
+      .get(path)
+      .then(() => {
+        console.log("잘 됨 ");
+      })
+      .catch((error) => {
+        const code = error.response.status;
+
+        switch (code) {
+          case 401:
+            console.log("401 응답 입니다. 로그인되지 않았습니다.");
+            break;
+          case 403:
+            console.log("403 응답 입니다. 권한이 없습니다.");
+            break;
+        }
+      })
+      .finally(() => {
+        // finally : 응답 코드가 200이든 아니든 항상 실행됨
+        console.log("항상 실행되는 메소드");
+      });
+  }
+
   return (
     <div>
       <button onClick={handle200}>200응답</button>
       <button onClick={handle400}>400응답</button>
       <button onClick={handle500}>500응답</button>
+
+      <br />
+      <button onClick={handleSome}>응답코드에 따라 다른 일 하기</button>
     </div>
   );
 }
