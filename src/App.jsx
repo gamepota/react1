fb20407f6e8102ba129fdb883430061ac0669ad2
Dongwon <kdw686968@gmail.com>
2024-05-16 10:16:01 +0900

    /api/someurl3의 get 요청 버튼 작성

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,15 @@ function App(props) {
     axios.get("/api/someurl2");
   }
 
+  function handleClickButton5() {
+    const params = new URLSearchParams();
+    params.append("id", 3);
+    params.append("name", "son");
+    params.append("email", "son@gmail.com");
+    // axios.get("/api/someurl3?" + params);
+    axios.get(`/api/someurl3?${params}`);
+  }
+
   return (
     <div>
       <button onClick={handleClickButton1}>get 요청</button>
@@ -25,6 +34,8 @@ function App(props) {
       <hr />
       <button onClick={handleClickButton3}>get2</button>
       <button onClick={handleClickButton4}>post2</button>
+      <hr />
+      <button onClick={handleClickButton5}> get 요청 시 query string</button>
     </div>
   );
 }
