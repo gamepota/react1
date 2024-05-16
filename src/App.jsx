4cfe005afdbb56c58087800aa2e996db184f68da
Dongwon <kdw686968@gmail.com>
2024-05-16 14:14:16 +0900

    then

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import axios from "axios";
 
 function App(props) {
-  return <div></div>;
+  function handleClick1() {
+    axios.get("/api/main42/sub1").then(() => {}); // then의 함수를 파라미터로 받음
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick1}>응답</button>
+    </div>
+  );
 }
 
 export default App;
