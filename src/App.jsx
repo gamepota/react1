2cddba6a44e085a77dfe3c621be238b0161195e2
Dongwon <kdw686968@gmail.com>
2024-05-16 10:05:42 +0900

    /api/someurl1,2의 get, post ajax 요청 버튼 작성

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import axios from "axios";
 
 function App(props) {
-  return <div></div>;
+  function handleClickButton1() {
+    axios.get("/api/someurl");
+  }
+
+  function handleClickButton2() {
+    axios.post("/api/someurl");
+  }
+
+  function handleClickButton3() {
+    axios.get("/api/someurl2");
+  }
+
+  function handleClickButton4() {
+    axios.get("/api/someurl2");
+  }
+
+  return (
+    <div>
+      <button onClick={handleClickButton1}>get 요청</button>
+      <button onClick={handleClickButton2}>post 요청</button>
+      <hr />
+      <button onClick={handleClickButton3}>get2</button>
+      <button onClick={handleClickButton4}>post2</button>
+    </div>
+  );
 }
 
 export default App;
