2baf7744e461f6777aeea07b9d5f6b5baff64d81
Dongwon <kdw686968@gmail.com>
2024-05-20 11:27:45 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import { Button, ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
   return (
     <ChakraProvider>
-      <Button sx={{ color: "orange" }} colorScheme={"blue"}>
-        Lorem.ff
-      </Button>
+      <div></div>
     </ChakraProvider>
   );
 }
